5bd598e077632068b9b40d2e74f0d6f6ed6a6158
Rodrigo Zepeda-Tello <RodrigoZepeda@users.noreply.github.com>
2021-05-26 22:32:16 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -11,27 +11,42 @@ raw_folder      = "raw"
 print("Descargando datos estatales")
 
 #Descarga del javascript donde están los datos
-url_js    = 'http://vacunacovid.gob.mx/wordpress/wp-content/themes/vacunamx/custom/js/home.js'
+url_js    = 'http://vacunacovid.gob.mx/wordpress'
 js_text   = requests.get(url_js, allow_redirects=True).text
 
 #Guardamos el raw
-file_js = open(raw_folder + "/home " + date.today().strftime('%Y-%m-%d') + ".js","w")
-file_js.writelines(js_text)
+file_js = open(raw_folder + "/" + date.today().strftime('%Y-%m-%d') + ".html","w")
+file_js.writelines(str(js_text.encode('utf-8')))
 file_js.close()
 
 #Limpiamos el texto para obtener el número de vacunados
-vaccine_data = re.split("let vacunas_data", js_text)[1]
-vaccine_data = re.split(";", vaccine_data, 1)[0]
+vaccine_data = re.sub("(<!--.*?-->)", "", js_text, flags=re.DOTALL)
+vaccine_data = re.split(" <svg xmlns:mapsvg=.*?>", vaccine_data)[1]
+vaccine_data = re.split("</svg>",vaccine_data, 1)[0]
 vaccine_data = vaccine_data.replace("\n","")
-vaccine_data = vaccine_data.replace(" ","")
-vaccine_data = "vacunas_data" + vaccine_data
+vaccine_data = re.split("<path d=.*?",vaccine_data)
+re.split('data-title=\".*?\"', vaccine_data[2])[1]
+vacunas = []
+for i in range(len(vaccine_data)):
 
-#Creamos la variable de vaccine data
-exec(vaccine_data)
+    #Get state name
+    match_title = re.search('data-title=\".*?\"', vaccine_data[i])
+    if match_title:
+        title = match_title.group(0).replace("data-title=","").replace("\"","")
+
+    #Get state number
+    match_vacunados = re.search('data-vacunados=\".*?\"', vaccine_data[i])
+    if match_vacunados:
+        vacunados = match_vacunados.group(0).replace("data-vacunados=", "").replace("\"", "").replace(",","")
+
+    if match_title and match_vacunados:
+        vacunas.append({"Estado": title, "vacunados": vacunados})
+        print(title + ": " + vacunados + " vacunados")
+
 
 #Parseamos el número de vacunados
-dfvacunas = pd.DataFrame.from_dict(vacunas_data)
-dfvacunas['vacunados'] = dfvacunas['vacunados'].str.replace(r',', '').astype(int)
+dfvacunas = pd.DataFrame.from_dict(vacunas)
+dfvacunas['vacunados'] = pd.to_numeric(dfvacunas['vacunados'])
 dfvacunas['descarga']  = datetime.now().strftime("%d-%b-%Y (%H:%M:%S)")
 
 #Guardamos
