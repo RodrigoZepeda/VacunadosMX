aff7b2bee9c979ed4a7279981609e6e8f11a5bea
RodrigoZepeda <rzepeda17@gmail.com>
2021-08-06 12:47:36 -0500

    Se actualizadon datos hasta 2021-08-06

---
+++ orchestrate.sh
@@ -0,0 +1,12 @@
+#-----------------------------
+#File for downloading info from the vaccine website, creating the database
+#and some plots to upload to github
+#Author: Rodrigo Zepeda
+#Contact: rzepeda17[at]gmail.com
+#----------------------------------------
+date=$(date '+%Y-%m-%d')
+python3 main.py
+python3 plots.py
+git add .
+git commit -m "Se actualizadon datos hasta ${date}"
+git push origin main
